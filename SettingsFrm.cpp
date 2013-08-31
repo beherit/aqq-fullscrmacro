410035d17ab1099a1a73f9b88f5e703f46857d25
Krzysztof Grochocki <beherit@users.noreply.github.com>
2013-08-31 21:43:11 +0200

    * Pierwsza publiczna wersja.

---
+++ SettingsFrm.cpp
@@ -0,0 +1,166 @@
+//---------------------------------------------------------------------------
+// Copyright (C) 2013 Krzysztof Grochocki
+//
+// This file is part of FullScrMacro
+//
+// FullScrMacro is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// FullScrMacro is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GNU Radio; see the file COPYING. If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Boston, MA 02110-1301, USA.
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#pragma hdrstop
+#include "SettingsFrm.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma link "sBevel"
+#pragma link "sButton"
+#pragma link "sSkinManager"
+#pragma link "sSkinProvider"
+#pragma link "sComboBox"
+#pragma link "acAlphaImageList"
+#pragma link "sMemo"
+#pragma link "sEdit"
+#pragma link "sSpinEdit"
+#pragma resource "*.dfm"
+TSettingsForm *SettingsForm;
+//---------------------------------------------------------------------------
+__declspec(dllimport)UnicodeString GetPluginUserDir();
+__declspec(dllimport)UnicodeString GetThemeSkinDir();
+__declspec(dllimport)bool ChkSkinEnabled();
+__declspec(dllimport)bool ChkThemeAnimateWindows();
+__declspec(dllimport)bool ChkThemeGlowing();
+__declspec(dllimport)int GetHUE();
+__declspec(dllimport)int GetSaturation();
+__declspec(dllimport)void LoadSettings();
+__declspec(dllimport)UnicodeString StrToIniStr(UnicodeString Str);
+__declspec(dllimport)UnicodeString IniStrToStr(UnicodeString Str);
+__declspec(dllimport)UnicodeString GetIconPath(int Icon);
+//---------------------------------------------------------------------------
+__fastcall TSettingsForm::TSettingsForm(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::WMTransparency(TMessage &Message)
+{
+  Application->ProcessMessages();
+  if(sSkinManager->Active) sSkinProvider->BorderForm->UpdateExBordersPos(true,(int)Message.LParam);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::FormCreate(TObject *Sender)
+{
+  //Wlaczona zaawansowana stylizacja okien
+  if(ChkSkinEnabled())
+  {
+	UnicodeString ThemeSkinDir = GetThemeSkinDir();
+	//Plik zaawansowanej stylizacji okien istnieje
+	if(FileExists(ThemeSkinDir + "\\\\Skin.asz"))
+	{
+	  //Dane pliku zaawansowanej stylizacji okien
+	  ThemeSkinDir = StringReplace(ThemeSkinDir, "\\\\", "\\", TReplaceFlags() << rfReplaceAll);
+	  sSkinManager->SkinDirectory = ThemeSkinDir;
+	  sSkinManager->SkinName = "Skin.asz";
+	  //Ustawianie animacji AlphaControls
+	  if(ChkThemeAnimateWindows()) sSkinManager->AnimEffects->FormShow->Time = 200;
+	  else sSkinManager->AnimEffects->FormShow->Time = 0;
+	  sSkinManager->Effects->AllowGlowing = ChkThemeGlowing();
+	  //Zmiana kolorystyki AlphaControls
+	  sSkinManager->HueOffset = GetHUE();
+	  sSkinManager->Saturation = GetSaturation();
+	  //Aktywacja skorkowania AlphaControls
+	  sSkinManager->Active = true;
+	}
+	//Brak pliku zaawansowanej stylizacji okien
+	else sSkinManager->Active = false;
+  }
+  //Zaawansowana stylizacja okien wylaczona
+  else sSkinManager->Active = false;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::FormShow(TObject *Sender)
+{
+  //Wczytanie ikon z interfesju AQQ
+  sAlphaImageList->AcBeginUpdate();
+  sAlphaImageList->Clear();
+  sAlphaImageList->LoadFromFile(GetIconPath(2));
+  sAlphaImageList->LoadFromFile(GetIconPath(1));
+  sAlphaImageList->LoadFromFile(GetIconPath(7));
+  sAlphaImageList->LoadFromFile(GetIconPath(3));
+  sAlphaImageList->LoadFromFile(GetIconPath(4));
+  sAlphaImageList->LoadFromFile(GetIconPath(5));
+  sAlphaImageList->LoadFromFile(GetIconPath(6));
+  //Odczyt ustawien
+  aLoadSettings->Execute();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::aExitExecute(TObject *Sender)
+{
+  Close();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::aLoadSettingsExecute(TObject *Sender)
+{
+  TIniFile *Ini = new TIniFile(GetPluginUserDir() + "\\\\FullScrMacro\\\\Settings.ini");
+  StateComboBox->ItemIndex = Ini->ReadInteger("Settings","State",5);
+  StatusMemo->Text = UTF8ToUnicodeString(IniStrToStr(Ini->ReadString("Settings","Status","").Trim().w_str()));
+  sSpinEdit->Value = Ini->ReadInteger("Settings","Delay",3);
+  delete Ini;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::aSaveSettingsExecute(TObject *Sender)
+{
+  TIniFile *Ini = new TIniFile(GetPluginUserDir() + "\\\\FullScrMacro\\\\Settings.ini");
+  Ini->WriteInteger("Settings","State",StateComboBox->ItemIndex);
+  ShortString pStatus = UTF8EncodeToShortString(StatusMemo->Text);
+  Ini->WriteString("Settings", "Status", StrToIniStr(pStatus.operator AnsiString()));
+  Ini->WriteInteger("Settings","Delay",sSpinEdit->Value);
+  delete Ini;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::StateComboBoxDrawItem(TWinControl *Control, int Index,
+          TRect &Rect, TOwnerDrawState State)
+{
+  //Dodanie ikonek do ComboBox
+  if(State.Contains(odSelected)) StateComboBox->Canvas->DrawFocusRect(Rect);
+  StateComboBox->Canvas->Brush->Style = bsClear;
+  sAlphaImageList->Draw(StateComboBox->Canvas,Rect.left+2,Rect.top+2,Index);
+  StateComboBox->Canvas->TextOutW(Rect.left+22,Rect.top+3,StateComboBox->Items->Strings[Index]);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::aSelectMemoExecute(TObject *Sender)
+{
+  StatusMemo->SelectAll();
+}
+//---------------------------------------------------------------------------
+void __fastcall TSettingsForm::OKButtonClick(TObject *Sender)
+{
+  //Zapisanie ustawien
+  aSaveSettings->Execute();
+  //Odczyt ustawien w rdzeniu
+  LoadSettings();
+  //Zamkniecie formy
+  Close();
+}
+//---------------------------------------------------------------------------
+
